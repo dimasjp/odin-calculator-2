087d6c6f3a4d345b68e9fbace19ca49b7503ba02
dimasjp <diims.el@gmail.com>
2025-08-12 08:14:41 +0700

    Add function operate

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let firstInput;
+let secondInput;
+let operator;
+
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +17,19 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(firstInput, secondInput, operator) {
+    switch (operator) {
+        case "+":
+            return add(firstInput, secondInput);
+        case "-":
+            return subtract(firstInput, secondInput);
+        case "*":
+            return multiply(firstInput, secondInput);
+        case "/":
+            return divide(firstInput, secondInput);
+        default:
+            return null;
+    }
 }
